da74bf8e8b165b90ed61b2f944b08494528cd065
theintegrative <theintegrative@gmail.com>
2022-11-26 00:56:14 +0100

    Added a multiprocessing for requests

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import requests 
+import concurrent.futures
+import time
+
+def requesting(count):
+    count + 1
+    print(f'Request {count}...')
+    x = requests.get('http://localhost:8080', stream=True)
+    return f'Request {count} done'
+
+def run_processes(total: int):
+    start = time.perf_counter()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = [executor.submit(requesting, count) for count in range(total)]
+    
+        for fu in concurrent.futures.as_completed(results):
+            print(fu.result())
+
+    finish = time.perf_counter()
+    print(f'Finished {total} request(s) in {round(finish-start, 2)} second(s)')
 
 if __name__ == '__main__':
-    for _ in range(10):
-        x = requests.get('http://localhost:8080')
+    run_processes(10000)
